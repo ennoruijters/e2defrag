e91faee4d5a73d8ec3d9d7d211d259b256a574ed
Enno Ruijters <ennoruijters@gmail.com>
2009-06-28 21:04:25 +0200

    Let allocate_blocks allocate more than one extent.
    It's still rather naive, because it always allocates the biggest extents
    it can find, but it's better than it was. Note that this is not very
    well tested and will probably reveal bugs in other parts of the program
    that worked correctly when only one extent could be returned. Beware
    of possible data corruption from this.

--- freespace.c
+++ freespace.c
@@ -1,5 +1,8 @@
 #include <malloc.h>
 #include <errno.h>
+#include <obstack.h>
+#define obstack_chunk_alloc malloc
+#define obstack_chunk_free free
 #include "e2defrag.h"
 #include "extree.h"
 
@@ -111,29 +114,79 @@ struct allocation *allocate_blocks(struct defrag_ctx *c, e2_blkcnt_t num_blocks,
                                    ext2_ino_t inode_nr, blk64_t first_logical)
 {
 	struct rb_node *n = rb_last(&c->free_tree_by_size);
-	struct free_extent *biggest = rb_entry(n, struct free_extent, size_rb);
-	struct allocation *ret;
-	int tmp;
-
-	if (biggest->end_block - biggest->start_block + 1 < num_blocks) {
+	if (!n) {
 		errno = ENOSPC;
 		return NULL;
 	}
-	ret = malloc(sizeof(struct allocation) + sizeof(struct data_extent));
+	struct free_extent *biggest = rb_entry(n, struct free_extent, size_rb);
+	struct allocation *ret, *tmp_alloc;
+	struct obstack stack;
+	size_t size;
+	int tmp, i, num_extents = 0;
+
+	obstack_init(&stack);
+	obstack_blank(&stack, sizeof(struct allocation));
+	while (num_blocks) {
+		struct data_extent new_extent;
+		e2_blkcnt_t biggest_num_blocks;
+		if (!biggest) {
+			errno = ENOSPC;
+			obstack_free(&stack, NULL);
+			return NULL;
+		}
+		new_extent.start_block = biggest->start_block;
+		new_extent.start_logical = first_logical;
+		new_extent.end_block = biggest->start_block - 1;
+		biggest_num_blocks = biggest->end_block - biggest->start_block;
+		biggest_num_blocks += 1;
+		if (biggest_num_blocks > num_blocks) {
+			new_extent.end_block += num_blocks;
+			num_blocks = 0;
+		} else {
+			new_extent.end_block += biggest_num_blocks;
+			num_blocks -= biggest_num_blocks;
+			first_logical += biggest_num_blocks;
+			n = rb_prev(n);
+			if (n == NULL) {
+				biggest = NULL;
+			} else {
+				biggest = rb_entry(n, struct free_extent,
+				                   size_rb);
+			}
+		}
+		new_extent.sparse = NULL;
+		new_extent.inode_nr = inode_nr;
+		obstack_grow(&stack, &new_extent, sizeof(new_extent));
+		num_extents++;
+	}
+	size = obstack_object_size(&stack);
+	ret = malloc(size);
 	if (!ret)
 		return NULL;
-	ret->extents[0].start_block = biggest->start_block;
-	ret->extents[0].end_block = biggest->start_block + num_blocks - 1;
-	ret->extents[0].start_logical = first_logical;
-	ret->extents[0].sparse = NULL;
-	ret->extents[0].inode_nr = inode_nr;
-	ret->block_count = num_blocks;
-	ret->extent_count = 1;
-	tmp = allocate_space(c, biggest->start_block, num_blocks);
+	tmp_alloc = obstack_finish(&stack);
+	memcpy(ret, tmp_alloc, size);
+	obstack_free(&stack, NULL);
+	ret->block_count = 0;
 
-	if (tmp < 0) {
-		free(ret);
-		return NULL;
+	for (i = 0; i < num_extents; i++) {
+		num_blocks = ret->extents[i].end_block + 1;
+		num_blocks -= ret->extents[i].start_block;
+		ret->block_count += num_blocks;
+		tmp=allocate_space(c, ret->extents[i].start_block, num_blocks);
+		if (tmp < 0)
+			goto error_dealloc;
 	}
+	ret->extent_count = num_extents;
 	return ret;
+
+error_dealloc:
+	; /* null statement because labels cannot precede declarations */
+	int j;
+	for (j = 0; j < i; j++) {
+		num_blocks = ret->extents[j].end_block + 1;
+		num_blocks -= ret->extents[j].start_block;
+		deallocate_space(c, ret->extents[j].start_block, num_blocks);
+	}
+	free(ret);
+	return NULL;
 }
