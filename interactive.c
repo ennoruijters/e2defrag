0ef942188fd6126c5da420552d2764fc35251d61
Enno Ruijters <ennoruijters@gmail.com>
2009-07-27 20:36:09 +0200

    Improved inode number input in interactive mode
    Replaced the scanf by a getnumber function that does proper handling
    of invalid input.

--- interactive.c
+++ interactive.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <limits.h>
 #include "e2defrag.h"
 #include "extree.h"
 
 extern int do_one_inode(struct defrag_ctx *c, ext2_ino_t, int);
+
+static int getnumber(long *ret, long min, long max)
+{
+	char *endptr;
+	long tmp;
+	char input[(sizeof(long) * CHAR_BIT) / 3 + 3];
+	/* Definitely large enough to hold LONG_MAX and LONG_MIN */
+
+	if (fgets(input, sizeof(input), stdin) == NULL)
+		return -1;
+	if (input[strlen(input) - 1] != '\n') {
+		do {
+			tmp = getchar();
+		} while (tmp != '\n' && tmp != EOF);
+	} else {
+		input[strlen(input) - 1] = '\0';
+	}
+	*ret = strtol(input, &endptr, 10);
+	if ((*ret == LONG_MAX || *ret == LONG_MIN) && errno == ERANGE)
+		return -1;
+	if (*ret > max || *ret < min) {
+		errno = ERANGE;
+		return -1;
+	}
+	if (*endptr != '\0') {
+		errno = EDOM;
+		return -1;
+	}
+	return 0;
+}
 
 static void print_fragged_inodes(const struct defrag_ctx *c)
 {
@@ -46,28 +77,46 @@ int defrag_file_interactive(struct defrag_ctx *c)
 	printf("Biggest free space: %llu blocks (%d)\n", biggest_size,
 	       num_biggest);
 	do {
+		long number;
 		printf("Specify inode number (or -1 for free space consolidation, or 0 to exit): ");
-		scanf("%u", &inode_nr);
-		if (inode_nr == 0)
+		if (getnumber(&number, -2, c->sb.s_inodes_count) < 0) {
+			switch (errno) {
+			case EDOM:
+				printf("Not a valid number\n");
+				break;
+			case ERANGE:
+				printf("Inode number out of range\n");
+				break;
+			default:
+				printf("Error: %s\n", strerror(errno));
+				return -1;
+			}
+			number = LONG_MIN;
+		}
+		switch (number) {
+		case 0:
 			return 1;
-		if (inode_nr == -1) {
+		case -1:
 			ret = consolidate_free_space(c, 0);
 			if (ret)
 				printf("Error: %s\n", strerror(errno));
 			if (ret && errno != ENOSPC)
 				return ret;
-			else
-				return 0;
-		}
-		if (inode_nr == -2) {
+			return 0;
+		case -2:
 			ret = 0;
 			while (!ret)
 				ret = consolidate_free_space(c, 0);
 			if (errno == ENOSPC)
 				return 0;
 			return ret;
+		case LONG_MIN:
+			inode_nr = 0;
+			break;
+		default:
+			inode_nr = number;
 		}
-	} while (inode_nr < 11 || inode_nr > c->sb.s_inodes_count);
+	} while (inode_nr < EXT2_FIRST_INO(&c->sb));
 	inode = c->inodes[inode_nr];
 	if (inode == NULL || inode->extent_count == 0) {
 		printf("Inode has no data associated\n");
