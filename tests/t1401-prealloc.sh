138487946bb44229341a17a4f88968fb0ac3093f
Enno Ruijters <ennoruijters@gmail.com>
2009-08-30 19:58:51 +0200

    Added test for fallocated (uninitialized) extents.
    It fails for now for obvious reasons (although in a much less obvious
    way).

---
+++ tests/t1401-prealloc.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Tests if a single file of one normal 1-block extent and one 1-block
+# uninitialized extent (and an unused block inbetween them) is correctly
+# defragmented on a tiny ext4 filesystem.
+
+. ./test-lib.sh
+
+test_begin "t1401-prealloc"
+
+load_image prealloc
+
+infra_cmd "mv prealloc.img disk.img"
+infra_cmd "echo \"dump_inode <12> before\nquit\n\" | debugfs disk.img \
+           > /dev/null"
+
+test_and_stop_on_error "defragmenting ext4 disk with uninitialized extent" \
+                       "e2defrag disk.img > /dev/null"
+
+test_and_continue "resulting image should not have file system errors" \
+                  "e2fsck -f -y disk.img 2>/dev/null > fsckout"
+
+test_and_continue "resulting image should not be fragmented" \
+                  "grep \"0.0% non-contiguous\" fsckout > /dev/null"
+
+infra_cmd "echo \"dump_inode <12> after\nquit\n\" | debugfs disk.img"
+
+# Note: This relies on debugfs dumping even the uninitialized extents. If this
+# ever changes we'll have to find some other way of testing this.
+test_and_continue "uninitialized blocks should not have been copied" \
+                  "! cmp -s -i 1024 before after"
+
+test_and_continue "initialized blocks should be unchanged" \
+                  "cmp -n 1024 before after"
+
+test_end
